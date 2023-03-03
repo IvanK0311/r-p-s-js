ba37d8077ed1858997f6b6628d417e8791667596
IvanK0311 <klajicivan311@gmail.com>
2023-03-03 19:17:48 +0100

    added UI

--- script.js
+++ script.js
@@ -2,70 +2,81 @@ let computerChoice
 let playerChoice
 let computerScore = 0
 let playerScore = 0 
+let rockBtn = document.getElementById("rock")
+let scissorsBtn = document.getElementById("scissors")
+let paperBtn = document.getElementById("paper")
+let winner = document.getElementById("declare-winner")
+let playerScoreDisplay = document.getElementById("player-score")
+let computerScoreDisplay = document.getElementById("computer-score")
+let choice = document.getElementById("choices")
+let restartBtn = document.getElementById("restart")
 
-function getPlayerChoice () {
-    return prompt("rock, paper or scissors?")
-}
+
 
 function getComputerChoice () {
-    computerChoice = Math.floor(Math.random() * 3) + 1
+    computerChoice = Math.floor(Math.random() * 3) + 1;
     if (computerChoice === 1) {
     return "rock";
 }  else if (computerChoice === 2) {
     return "paper";
 } else {
     return "scissors";
-}
-}
+};
+};
 
-function playRound (playerChoice, computerChoice) {
-    playerChoice = playerChoice.toLowerCase()
-    if (playerChoice === computerChoice) {
-        alert(`Tie! Player score : ${playerScore}. Computer score : ${computerScore}`)
-    } else if (playerChoice === "rock" && computerChoice === "scissors") {
-        playerScore ++
-        alert(`You win! Player score : ${playerScore}. Computer score : ${computerScore}`)
+function playRound (playerChoice) {
+    computerChoice = getComputerChoice();
+    if (playerChoice === "rock" && computerChoice === "scissors") {
+        playerScore ++;
     } else if (playerChoice === "paper" && computerChoice === "rock") {
-        playerScore ++
-        alert(`You win! Player score : ${playerScore}. Computer score : ${computerScore}`)
+        playerScore ++;
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
-        playerScore ++
-        alert(`You win! Player score : ${playerScore}. Computer score : ${computerScore}`)
-    } else {
-        computerScore ++ 
-        alert(`You loose! Player score : ${playerScore}. Computer score : ${computerScore}`)
+        playerScore ++;
+    } else if (playerChoice === "rock" && computerChoice === "paper") { 
+        computerScore ++;
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        computerScore ++;
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        computerScore ++;
     }
- }
+
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    choice.textContent = `You picked ${playerChoice} and the computer picked ${computerChoice}.`;
+
+    if (computerScore === 5) {
+        winner.textContent = "The computer beat you! Do you want to restart the game?";
+        restartBtn.style.display ="inline"
+      } else if (playerScore === 5) {
+        winner.textContent = "You beat the computer! Do you want to restart the game?";
+        restartBtn.style.display ="inline"
+      };
+}      
 
  function restartGame () {
-    alert(`The game restarted, good luck!`)
-    playerScore = 0
-    computerScore = 0
-    playGame()
- }
+    playerScore = 0;
+    computerScore = 0;
+    choice.textContent = "";
+    winner.textContent = "";
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+    restartBtn.style.display = "none";
 
-function playGame() {
-    while (playerScore < 5 && computerScore < 5) {
-    playRound(getPlayerChoice(), getComputerChoice())
-    }
-    if (playerScore === 5) {
-        alert(`Congratulations! You beat the computer!`)
-        let restart = confirm(`Do you want to restart the game?`)
-        if (restart) {
-          restartGame()  
-        }
-    } else {
-        alert(`The computer beat you! Better luck next time!`)
-        let restart = confirm(`Do you want to restart the game?`)
-        if (restart) {
-          restartGame()  
-        }
-        
-    }
-}
- 
-alert(`The first one to reach 5 points wins! Best of luck!`)
+ };
 
-playGame()
+ rockBtn.addEventListener("click", function() {
+    playRound("rock");
+  });
 
+  paperBtn.addEventListener("click", function() {
+    playRound("paper");
+  });
+
+  scissorsBtn.addEventListener("click", function() {
+    playRound("scissors");
+  });
+
+  restartBtn.addEventListener("click", function() {
+    restartGame();
+  })
 
